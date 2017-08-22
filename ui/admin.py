1af2ed9f9da3fcdeb47f09c0b6dbe243977bd49a
Shortylo85 <daneshorty@gmail>
2017-08-22 02:58:18 +0200

    added city model, prepared for importing of csv data

--- ui/admin.py
+++ ui/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from ui.models import City
+
+
 # Register your models here.
+admin.site.register(City)
--- ui/models.py
+++ ui/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.fields.related import ForeignKey
+
 
 # Create your models here.
+class City(models.Model):
+    city_name = models.CharField(max_length=64, null=True, blank=True)
+    user = ForeignKey(User)
